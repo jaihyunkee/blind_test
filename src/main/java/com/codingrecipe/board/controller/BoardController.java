f607344dc3038bac53f7762edb8b2fbb9362ec78
Jay kee <jkee@purdue.edu>
2024-11-14 19:15:59 +0900

    Day 1

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,73 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        int saveResult = boardService.save(boardDTO);
+        if (saveResult > 0) {
+            return "redirect:/board/";
+        }
+        else {
+            return "save";
+        }
+
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";  // Make sure 'detail' view exists
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id, Model model) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model){
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+        return "detail";
+
+    }
+
+}
